dbfd199e97a54a3398c09ca3dea7d022b7cfd80d
KeithY14 <keithyong14@yahoo.com.sg>
2021-10-21 16:26:59 +0800

    Add in additional test case

--- 28_KeithY_Lab10/Assets/Tests/TestSuite.cs
+++ 28_KeithY_Lab10/Assets/Tests/TestSuite.cs
@@ -17,20 +17,6 @@ namespace Tests
             Assert.AreEqual(result, 7);
         }
 
-        [Test]
-        public void TestSuitSimplePasses()
-        {
-            result = Calculator.CalculatePair(5, 2, "+");
-            Assert.AreEqual(result, 7);
-        }
-
-        [Test]
-        public void TestSuitWithEnumeratorPasses()
-        {
-            result = Calculator.CalculatePair(5, 2, "+");
-            Assert.AreEqual(result, 7);
-        }
-
         [UnityTest]
         public IEnumerator TestUnityAddition()
         {
@@ -40,5 +26,68 @@ namespace Tests
             Assert.AreEqual(result, 7);
         }
 
+        [Test]
+        public void TestSubtraction()
+        {
+            result = Calculator.CalculatePair(100, 90, "-");
+            Assert.AreEqual(result, 10);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnitySubtraction()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(100, 90, "-");
+            Assert.AreEqual(result, 10);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            result = Calculator.CalculatePair(4, 3, "*");
+            Assert.AreEqual(result, 12);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityMultiplication()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(4, 3, "*");
+            Assert.AreEqual(result, 12);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            result = Calculator.CalculatePair(1000, 200, "/");
+            Assert.AreEqual(result, 5);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityDivision()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(1000, 200, "/");
+            Assert.AreEqual(result, 5);
+        }
+
+        [Test]
+        public void TestSquareRoot()
+        {
+            result = Calculator.CalculatePair(25, 5, "/");
+            Assert.AreEqual(result, 5);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnitySquareRoot()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(25, 5, "/");
+            Assert.AreEqual(result, 5);
+        }
     }
 }
